3671085c9399035aa0b0aad877095beb8f209c34
LuxARTS <bacelolucas@gmail.com>
2020-02-21 11:41:50 -0300

    Add note function

--- Core/Inc/midi.h
+++ Core/Inc/midi.h
@@ -11,16 +11,18 @@
 #include "stdint.h"
 #include "usbd_cdc_if.h"
 
-#define STATUS_NOTE_OFF	0x8
-#define STATUS_NOTE_ON	0x9
-#define STATUS_CC		0xB
+#define STATUS_NOTE_OFF	0x80
+#define STATUS_NOTE_ON	0x90
+#define STATUS_CC		0xB0
 
 typedef struct{
-	uint8_t status;
+	uint8_t type;
+	uint8_t channel;
 	uint8_t data1;
 	uint8_t data2;
 } MIDI_msg_TypeDef;
 
 void MIDI_send(MIDI_msg_TypeDef *msg);
+void MIDI_note(uint8_t channel, uint8_t note, uint8_t velocity, uint8_t status);
 
 #endif /* INC_MIDI_H_ */
--- Core/Src/midi.c
+++ Core/Src/midi.c
@@ -9,13 +9,30 @@
 void MIDI_send(MIDI_msg_TypeDef *msg){
 	uint8_t buf[3];
 
-	// Prepare buffer
-	buf[0]= msg->status | 0x80; // Set bit 7
-	buf[1]= msg->data1 & 0x7F;  // Clean bit 7
-	buf[2]= msg->data2 & 0x7F;  // Clean bit 7
+	// Check if status type is valid
+	if(msg->type & 0x80){
+		// Prepare buffer
+		buf[0]= (msg->type & 0xF0) | (msg->channel & 0x0F);
+		buf[1]= msg->data1 & 0x7F;  // Clean bit 7
+		buf[2]= msg->data2 & 0x7F;  // Clean bit 7
 
-	// Send to USB
-	CDC_Transmit_FS(buf, 3);
+		// Send to USB
+		CDC_Transmit_FS(buf, 3);
+	}
 }
+void MIDI_note(uint8_t channel, uint8_t note, uint8_t velocity, uint8_t status){
+	static MIDI_msg_TypeDef msg = {0};
 
+	if(status){
+		msg.type = STATUS_NOTE_ON;
+		msg.data2 = velocity;
+	} else {
+		msg.type = STATUS_NOTE_OFF;
+		msg.data2 = 0;
+	}
 
+	msg.channel = channel;
+	msg.data1 = note;
+
+	MIDI_send(&msg);
+}
